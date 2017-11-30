445d80d3e2018e8995890b85c19cb52aada46843
1224735298 <1224735298@qq.com>
2017-11-30 15:22:43 +0800

    大师傅士大夫asdadaas

--- src/main/java/com/accp/entity/User.java
+++ src/main/java/com/accp/entity/User.java
@@ -2,5 +2,21 @@ package com.accp.entity;
 
 public class User {
     private String name;
+ private Integer xxx;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getXxx() {
+        return xxx;
+    }
+
+    public void setXxx(Integer xxx) {
+        this.xxx = xxx;
+    }
 }
